65117b8a2ff90bbdddb51dce7af864387098a059
Luigiv29 <luigiv3@nycstudents.net>
2026-01-13 17:33:19 -0500

    finshed

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
 let totalInCent = 0;
 
-const Buttonsticker = document.querySelector("#sticker");
+
+const stickerButton = document.querySelector("#sticker");
 const keychainButton = document.querySelector("#keychain");
 const plushButton = document.querySelector("#plush");
 const comicButton = document.querySelector("#comic");
-const mystery = document.querySelector("#mystery")
+const mysteryButton = document.querySelector("#mystery");
+
 
 const shoppingCart = document.querySelector("#shopping-cart");
 const totalSpan = document.querySelector("#total-span");
 
 const updateTotal = () => {
-    totalSpan.textContent = totalInCent / 100;
-}
+    totalSpan.textContent = (totalInCent / 100).toFixed(2);
+};
 
-const addApple = () => {
-    shoppingCart.innerHTML += `<p> Apple </p>`;
-    totalInCent += 75;
+
+const addSticker = () => {
+    shoppingCart.innerHTML += `<p>Sticker</p>`;
+    totalInCent += 50;
     updateTotal();
-}
-const addMango = () => {
-    shoppingCart.innerHTML += `<p> Mango </p>`;
-    totalInCent += 125;
+};
+
+const addKeychain = () => {
+    shoppingCart.innerHTML += `<p>Keychain</p>`;
+    totalInCent += 150;
     updateTotal();
-}
-const addBanana = () => {
-    shoppingCart.innerHTML += `<p> Banana </p>`;
-    totalInCent += 30;
+};
+
+const addPlush = () => {
+    shoppingCart.innerHTML += `<p>Plush</p>`;
+    totalInCent += 400;
     updateTotal();
-}
+};
+
+const addComic = () => {
+    shoppingCart.innerHTML += `<p>Comic</p>`;
+    totalInCent += 275;
+    updateTotal();
+};
+
+const addMystery = () => {
+    shoppingCart.innerHTML += `<p>Mystery Item</p>`;
+    totalInCent += 325;
+    updateTotal();
+};
 
 // Event Listeners
-
-appleButton.addEventListener("click", addsticker);
-mangoButton.addEventListener("click", addkeychain);
-bananaButton.addEventListener("click", addplush);
+stickerButton.addEventListener("click", addSticker);
+keychainButton.addEventListener("click", addKeychain);
+plushButton.addEventListener("click", addPlush);
+comicButton.addEventListener("click", addComic);
+mysteryButton.addEventListener("click", addMystery);
